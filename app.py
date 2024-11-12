3fd583042a9b8815a701b6876fc6ae59b5f01c7e
ai-jon <gillis.15.jg@gmail.com>
2024-11-11 19:22:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-# Initialize the Flask app
 app = Flask(__name__)
 
-# Define a route
 @app.route("/")
 def home():
     return "Hello, UFO Sightings!"
 
-# Run the app (optional, mainly for local testing)
 if __name__ == "__main__":
     app.run(debug=True)
